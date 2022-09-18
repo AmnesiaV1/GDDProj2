df35cbf27af36b656097e22817bf8632b32a6ab3
Oliver <odpetrick@berkeley.edu>
2022-09-18 15:20:54 -0700

    baseball win trigger script

---
+++ Assets/Scripts/BaseballWinTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baseball : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            //Todo: set up game manager.
+            //GameObject gm = GameObject.FindWithTag("GameController");
+            //gm.GetComponent<GameManager>().WinGame();
+        }
+    }
+}
