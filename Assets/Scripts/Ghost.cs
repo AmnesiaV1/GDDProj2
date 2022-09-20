002f7799c2f1b417b4400f48e1ebba52390499c0
Oliver <odpetrick@berkeley.edu>
2022-09-19 17:09:58 -0700

    update ghost script

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -6,9 +6,16 @@ public class Ghost : MonoBehaviour
 {
     #region Movement_variables
     [SerializeField]
+    [Tooltip("The distance from the player when the ghosts starts speeding up.")]
+    private float speedUpDistance;
+    [SerializeField]
+    [Tooltip("Scale factor that determins how much the ghost will speed up when close enough to the player.")]
     private float moveSpeedScaler;
     [SerializeField]
-    private float moveSpeedCapacity;
+    [Tooltip("Constant speed outside of range of player")]
+    private float moveSpeedOutside;
+    [SerializeField]
+    private float maxSpeed;
     float moveSpeed;
     #endregion
 
@@ -41,8 +48,16 @@ public class Ghost : MonoBehaviour
         //gets vector between ghost and player
         Vector2 direction = player.transform.position - transform.position;
 
-        //movespeed decreases as the ghost gets closer to the player but maxs out at Capactiy.
-        moveSpeed = Mathf.Max(direction.magnitude / moveSpeedScaler, moveSpeedCapacity);
+        if (direction.magnitude > speedUpDistance)
+        {
+            //if the ghost is outside of the speed up radius, then the ghost has a constant speed.
+            moveSpeed = moveSpeedOutside;
+        }
+        else
+        {
+            //As the ghost gets close, its speed will increase.
+            moveSpeed = Mathf.Min(maxSpeed, 1 / (direction.magnitude / moveSpeedScaler));
+        }
 
         GhostRB.velocity = direction.normalized * moveSpeed;
     }
